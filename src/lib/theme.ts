332e0b4220c0503c56c86fc0fbef887b38bfdac7
Rafael Willen <rafaelpadre20@gmail.com>
2024-08-13 23:49:44 +0100

    Add base theme

---
+++ src/lib/theme.ts
@@ -0,0 +1,15 @@
+export const theme = {
+  colors: {
+    primary100: "#01080e",
+    primary200: "#011627",
+    primary300: "#011221",
+    secondary100: "#607b86",
+    secondary200: "#3c9d93",
+    secondary300: "#4d5bce",
+    accent100: "#fea55f",
+    accent200: "#43d9ad",
+    accent300: "#e99287",
+    accent400: "#c98bdf",
+    lines: "#1e2d3d",
+  },
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,19 @@
+import { RiHeartFill } from "@remixicon/react";
 import { Div } from "@stylin.js/elements";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
-  component: () => <Div>Hello World!</Div>,
+  component: () => (
+    <Div
+      color="#fff"
+      display="flex"
+      gap="10px"
+      alignItems="center"
+      justifyContent="center"
+      fontSize="3rem"
+      height="100vh"
+    >
+      Hello World! <RiHeartFill size={48} />
+    </Div>
+  ),
 });
